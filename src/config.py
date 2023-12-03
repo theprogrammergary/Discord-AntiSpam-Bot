649beabd695ef5077330b3846147282d1a22e904
theprogrammergary <theprogrammergary@gmail.com>
2023-12-03 16:44:05 -0500

    feat: anti-spam

--- src/config.py
+++ src/config.py
@@ -7,3 +7,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # imposter
 MOD_ROLE_NAME = os.getenv("MOD_ROLE_NAME")
 MOD_LOG_CHANNEL_NAME = os.getenv("MOD_LOG_CHANNEL_NAME")
+
+# verify
+VERIFY_CHANNEL = os.getenv("VERIFY_CHANNEL")
+VERIFIED_ROLE = os.getenv("VERIFIED_ROLE")
+UNVERIFIED_ROLE = os.getenv("UNVERIFIED_ROLE")
