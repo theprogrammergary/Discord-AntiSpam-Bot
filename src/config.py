1bc99334c942408b207ae613699024fb2553f916
theprogrammergary <theprogrammergary@gmail.com>
2023-12-03 09:29:29 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,5 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(os.getcwd(), ".setup", ".env"))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# imposter
 MOD_ROLE_NAME = os.getenv("MOD_ROLE_NAME")
 MOD_LOG_CHANNEL_NAME = os.getenv("MOD_LOG_CHANNEL_NAME")
