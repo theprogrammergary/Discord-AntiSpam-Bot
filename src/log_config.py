aded2fb5624122f5dc3387c9f3ce16ad765965f8
theprogrammergary <theprogrammergary@gmail.com>
2023-12-05 21:02:43 -0500

    Create log_config.py

---
+++ src/log_config.py
@@ -0,0 +1,19 @@
+"""
+Configure logging for app
+"""
+
+# imports
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sink="./logs/{time:YYYY-MM-DD}.log",
+    rotation="00:00",
+    retention="14 days",
+    compression="zip",
+    backtrace=True,
+    format=(
+        "\n{time:YYYY-MM-DD HH:mm:ss} {level.icon} {level} \n"
+        '{file}>"{function}">{line} \n    {message} \n'
+    ),
+)
